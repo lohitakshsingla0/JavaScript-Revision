b1da86ffcd7f38277b5df46635bcf2beebff39b8
Lohitaksh Singla <lohitakshsingla0@gmail.com>
2022-04-27 15:48:54 +0200

    Create o8filterArrays.js

---
+++ o2Basics/o8filterArrays.js
@@ -0,0 +1,12 @@
+var testArray = [2, 4, 6, 8, 1, 5, 8, 9];
+
+// console.log(testArray.fill("h"));
+// console.log(testArray.fill("h", 0));
+console.log(testArray.fill("h", 2 ,5));
+
+
+const myNumber = [22,23,25,54,87,65,29,44];
+
+const result = myNumber.filter( (num) => (num%2==0) );
+
+console.log(result);
